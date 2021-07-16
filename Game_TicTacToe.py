ec42e91eae403c682e0ac556483545dda1bac360
LeslieDanill <87510124+LeslieDanill@users.noreply.github.com>
2021-07-16 16:11:16 -0500

    Jean add TTTgame

--- Game_TicTacToe.py
+++ Game_TicTacToe.py
@@ -1,32 +1,17 @@
 # --- Tic-Tac-Toe Game --- #
 
-# --- Define Game Board --- #
+# --- Setup Game --- #
+# Define game board
 board = {'A1': ' ', 'A2': ' ', 'A3': ' ', 'B1': ' ', 'B2': ' ', 'B3': ' ', 'C1': ' ', 'C2': ' ', 'C3': ' '}
+
+# Define Victory
+# !!! Can specify horizontal, vertical, diagonal
+victory = [['A1', 'A2', 'A3'], ['B1', 'B2', 'B3'], ['C1', 'C2', 'C3'], ['A1', 'B1', 'C1'], ['A2' , 'B2' , 'C2'], ['A3' , 'B3' , 'C3'], ['A1' , 'B2' , 'C3'], ['C1' , 'B2' , 'A3']]
 
 # --- Define Functions --- #
 # !!! Could loop through w function
-'''
-def getuserdifficulty():
-    userdifficulty = input()
-'''
-''' This is where you lost your patience
-def getusermove():
-    usermove = input()
-    #Validate
-    if usermove.upper() == 'X' or 'O':
-        ''
-    else:
-        print('Please choose either  X  or  O')
-        continue
-'''
 
-def getbotmove():
-    botmove = ''
-
-def updatenewmove():
-    newmove = ''
-
-def printboard():
+def printboard(board):
     while True:
         x = 'X'
         print('   | A | B | C |')
@@ -39,72 +24,31 @@ def printboard():
         print('   -------------')
         break
 
-# --- GAMEPLAY --- #
-
-#Bot message:  Welcome to TIC-TAC-TOE!  Choose your player:<br> X  or  O  ?
-# getusermove
-# validateusermove
-
-# input playermove
-# validate playermove, else input again; check if won or lost, and print and end game
-
-
-
+def gameplay():
+    print('Welcome to TIC-TAC-TOE!  Choose your player: \nX  or  O  ?')
+    usermove = input()
+    # input playermove
+    # validate playermove, else input again; check if won or lost, and print and end game
+    while True:
+        usermove = input()
+        #Validate
+        if usermove.upper() == 'X' or 'O':
+            ''
+        else:
+            print('Please choose either  X  or  O')
+            continue
 
 '''
-# --- Imports --- #
-import random
 
-# --- Define Functions --- #
-# Input & validate userguess
-def validateuserguess(): 
-    print('Take a guess.')
-    while True:
-        try:
-            userguess = int(input())
-            return userguess
-        except ValueError:
-            print('I\'m not your pal, buddy.')
-            print('Try a number this time?')
-            continue
-        except:
-            print('Beep boop. You crashed the game.')
-            exit(1) 
 
-# --- Start Game --- #
-print('Hello. What is your name?')
-name = input()
-key = random.randint(1,20)
-print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+# --- GAMEPLAY --- #
+gameplay()
 
-#Allow user to guess up to 6 times
-for x in range(6):
-    userguess = validateuserguess()
-    # If guess correct
-    if userguess == key:
-        print('Congrats, ' + name + ' ! You got it in only ' + str(x + 1) + ' ' + ("guesses" if (x + 1) > 1 else "guess") + '.')
-        print("And remember the lesson you've learned here... you can use double quotes any time you like friend ;)")
-        break
-    # If guess not in range
-    elif userguess < 1 or userguess > 20:
-        print('That\'s not even between 1 and 20 ...')
-    # If too low
-    elif userguess < (key - 10):
-        print('That\'s way too low!')
-    elif userguess < (key - 2):
-            print('Too low.')
-    elif userguess < key:
-        print('That\'s a little too low ...')
-    # If too high
-    elif userguess > (key + 10):
-        print('That\'s way too high!')
-    elif userguess > (key + 3):
-            print('Too high.')
-    elif userguess > key:
-        print('That\'s a little too high ...')
-    # If out of guesses
-    if (x + 1) == 6:
-        print('Uh oh, out of guesses. The number I was thinking of was ' + str(key) + '.')
 
-print('Bye bye now!')
+# --- Later, when you want to overcomplicate things, you can scale this game to Bingo... and more! --- #
+boarddimensions = {'x'='3', 'y'='3', 'z'='0'}
+board = 
+    int(boarddimensions['x'])
+
+
 '''
---
+++ throwaway.py
@@ -0,0 +1,1 @@
+print('Welcome to TIC-TAC-TOE!  Choose your player: \n X  or  O  ?')
